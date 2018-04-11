a97d67f2cffa5ea2431bb93ff03a041d7ac9b817
Imma7 <immamugambi@gmail.com>
2018-04-11 17:43:15 +0300

    Added Google Maps

--- js/scripts.js
+++ js/scripts.js
@@ -44,11 +44,11 @@ function showPosition(position) {
 }
 
 
-// function myMap() {
-//     var mapOptions = {
-//         center: new google.maps.LatLng(51.5, -0.12),
-//         zoom: 10,
-//         mapTypeId: google.maps.MapTypeId.HYBRID
-//     }
-// var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-// }
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
