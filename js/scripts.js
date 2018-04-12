caceaa8693314553b1a2b508813bb5ba4ffe5486
Imma7 <immamugambi@gmail.com>
2018-04-12 09:29:37 +0300

    Added Social Media Pages

--- js/scripts.js
+++ js/scripts.js
@@ -28,6 +28,8 @@
 //     alert("alert");
 // });
 
+
+// Get Current Position
 var x = document.getElementById("demo");
 
 function getLocation() {
@@ -43,10 +45,29 @@ function showPosition(position) {
     "<br>Longitude: " + position.coords.longitude;
 }
 
+// Show error if position doesn't show
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
 
+
+// Google Maps
 function myMap() {
     var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.12),
+        center: new google.maps.LatLng(-1.3003866, 36.7844459),
         zoom: 10,
         mapTypeId: google.maps.MapTypeId.HYBRID
     }
